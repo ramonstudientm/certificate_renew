53aac2176c52e0502038b7be0ccc2656b299f17b
Ramon <ramonstudientm@gmail.com>
2024-12-24 15:50:39 -0300

    Feito algumas adições no codigo, não me lembro exatamente o que, mas foi feito =)

--- my-app/app.py
+++ my-app/app.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import pandas as pd
 import logging, sys, ssl, socket
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -10,8 +11,10 @@ class Certificate:
         self.expDate = expDate
         self.creationDate = creationDate
     
-    def __str__(self):
-        return f"Common Name: {self.commonName}, Alt Name: {self.altName}, Exp Date: {self.expDate}, Creation Date: {self.creationDate}"
+    #def __str__(self):
+    #    return f"Common Name: {self.commonName}, Alt Name: {self.altName}, Exp Date: {self.expDate}, Creation Date: {self.creationDate}"
+    def to_dict(self):
+        return { 'Common Name': self.commonName, 'Alternative Names': ', '.join(self.altName) if self.altName else 'None', 'Expiration Date': self.expDate, 'Creation Date': self.creationDate }
 
 def get_domains_list(file_path):
     with open(file_path) as file:
@@ -21,24 +24,18 @@ def get_certificate_info(domain):
     context = ssl.create_default_context()
     context.check_hostname = True
     context.verify_mode = ssl.CERT_REQUIRED
-
     try:
         with context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=domain) as sock:
             sock.settimeout(4)
             sock.connect((domain, 443))
             cert = sock.getpeercert()
-            
-            # Common Name (CN)
+
             common_name = dict(x[0] for x in cert['subject'])['commonName']
-            # Subject Alternative Names (SAN)
             altName = [x[1] for x in cert.get('subjectAltName', [])]
-            # Expiration Date
             not_after = cert['notAfter']
             expDate = datetime.strptime(not_after, '%b %d %H:%M:%S %Y %Z')
-            # Creation Date
             not_before = cert['notBefore']
             creationDate = datetime.strptime(not_before, '%b %d %H:%M:%S %Y %Z')
-            
             return Certificate(common_name, altName, expDate, creationDate)
         
     except ssl.SSLError as e:
@@ -52,13 +49,29 @@ def get_certificate_info(domain):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        logging.error("Uso: python script.py domain.list")
+        logging.error("Uso: python script.py domain.list ")
         sys.exit(1)
     
-    list_domain = sys.argv[1]
-    domains = get_domains_list(list_domain)
+list_domain = sys.argv[1]
+domains = get_domains_list(list_domain)
     
-    for domain in domains:
-        cert_info = get_certificate_info(domain)
-        if cert_info:
-            logging.info(cert_info)
+    
+
+csv_name = 'default.csv'
+try:
+    if len(sys.argv) >= 2:
+        csv_name = sys.argv[2]
+except:
+    pass     
+        
+certificates = []
+for domain in domains:
+    cert_info = get_certificate_info(domain) 
+    if cert_info:
+        logging.info(cert_info)
+        print(cert_info)
+        certificates.append(cert_info.to_dict())
+        df = pd.DataFrame(certificates)
+        df.to_csv(csv_name, index=False)
+        logging.info(f"Informações dos certificados exportadas para '{csv_name}'")
+
